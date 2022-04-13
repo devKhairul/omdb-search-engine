52cc94daa3b0d1c2039f098d3521a0dffa626d2d
Khairul Alam <devKhairul@users.noreply.github.com>
2022-04-12 21:19:30 -0400

    Updated API URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=f28cb298';
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=f28cb298';
 
 const App = () => {
 
